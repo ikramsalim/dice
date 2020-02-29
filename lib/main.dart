6e8e2918f6acd247cb6600087442814934e75332
ikram <ikramomar998@gmail.com>
2020-02-29 15:56:50 -0600

    Used +1 since we can't have a dice number/image called 0 leftDiceNumber = Random().nextInt(6) + 1; Imported the math library to randomize the dice images from 1-6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -36,7 +37,7 @@ class _DicePageDynamicState extends State<DicePageDynamic> {
             child: FlatButton(
                 onPressed: () {
                   setState(() {
-                    leftDiceNumber = 5;
+                    leftDiceNumber = Random().nextInt(6) + 1;
                   });
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png')),
